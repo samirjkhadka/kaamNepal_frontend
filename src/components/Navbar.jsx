d47bf42424e4b57111bd7c92f3c7ab92da3fb4a5
samirjkhadka <findsamirkhadka@gmail.com>
2025-02-04 15:44:22 +0545

    Navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,35 @@
-import { useClerk } from "@clerk/clerk-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { openSignIn } = useClerk();
+  const { user } = useUser();
 
   return (
     <div className="shadow py-4 sticky">
       <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center ">
         <img src={assets.logo} alt="" className="" />
-        <div className="flex gap-4 max-sm:text-xs">
-          <button className="text-gray-600 bg-gray-200 px-6 sm:px-9 py-2 rounded-full">
-            Recruiter Login
-          </button>
-          <button
-            onClick={(e) => openSignIn()}
-            className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full"
-          >
-            Login
-          </button>
-        </div>
+        {user ? (
+          <div className="flex items-center gap-3">
+            <Link to={"/applications"}>Applied Jobs</Link>
+            <p>|</p>
+            <p>Hi, {user.firstName + " " + user.lastName}</p>
+            <UserButton />
+          </div>
+        ) : (
+          <div className="flex gap-4 max-sm:text-xs">
+            <button className="text-gray-600 bg-gray-200 px-6 sm:px-9 py-2 rounded-full">
+              Recruiter Login
+            </button>
+            <button
+              onClick={(e) => openSignIn()}
+              className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full"
+            >
+              Login
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
