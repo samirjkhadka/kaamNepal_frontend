b4c7666332840ba93140f66b4d682006e9109af3
samirjkhadka <findsamirkhadka@gmail.com>
2025-02-04 16:29:09 +0545

    Hero component complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div className="">
       <Navbar />
+      <Hero/>
     </div>
   );
 };
