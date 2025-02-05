070ec54a8b8f40bb38d63f2d3aa0f0c0919217a8
samirjkhadka <findsamirkhadka@gmail.com>
2025-02-05 20:15:45 +0545

    mobile nav complete job Card complete

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
 import { assets, JobCategories, JobLocations } from "../assets/assets";
 
@@ -7,6 +7,7 @@ import JobCard from "./JobCard";
 const JobListing = () => {
   const { isSearched, searchFilter, setSearchFilter, jobs } =
     useContext(AppContext);
+  const [showFilter, setShowFilter] = useState(false);
 
   return (
     <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
@@ -47,8 +48,16 @@ const JobListing = () => {
               </div>
             </>
           )}
+
+        <button
+          onClick={(e) => setShowFilter((prev) => !prev)}
+          className="px-6 py-1.5 rounded border border-gray-400 lg:hidden"
+        >
+          {showFilter ? "Close" : "Show Filters"}
+        </button>
+
         {/* category filter */}
-        <div className="max-lg:hidden">
+        <div className={showFilter ? "" : "max-lg:hidden"}>
           <h4 className="font-medium text-lg py-4">Search by Categories</h4>
           <ul className="space-y-4 text-gray-600">
             {JobCategories.map((category) => (
@@ -60,7 +69,7 @@ const JobListing = () => {
           </ul>
         </div>
         {/* Location filter */}
-        <div className="max-lg:hidden">
+        <div className={showFilter ? "" : "max-lg:hidden"}>
           <h4 className="font-medium text-lg py-4 pt-14">Search by Location</h4>
           <ul className="space-y-4 text-gray-600">
             {JobLocations.map((location) => (
